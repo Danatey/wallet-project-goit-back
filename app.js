507fc0d2867f40cc5f48e4a90f13dc12fe690a50
andriy-lanyk <LAM1988@ukr.net>
2021-11-13 10:38:54 +0200

    Create first version of documentation

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const swaggerUi = require('swagger-ui-express');
 
-const transactionsRouter = require("./routes/transactions/transactions");
-const usersRouter = require("./routes/users/users");
-const { HttpCode } = require("./config/contants");
+const transactionsRouter = require('./routes/transactions/transactions');
+const usersRouter = require('./routes/users/users');
+const { HttpCode } = require('./config/contants');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const swaggerDocument = require('./swagger.json');
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 // app.use(express.static());
 app.use(helmet());
@@ -17,20 +19,21 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json({ limit: 10000 }));
 
-app.use("/api/users", usersRouter);
-app.use("/api/transactions", transactionsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/transactions', transactionsRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res
     .status(HttpCode.FORBIDDEN)
-    .json({ status: "error", code: HttpCode.FORBIDDEN, message: "Not found" });
+    .json({ status: 'error', code: HttpCode.FORBIDDEN, message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
   console.log(`error`, err);
   const statusCode = err.status || HttpCode.INTERNAL_SERVER_ERROR;
   res.status(statusCode).json({
-    status: statusCode === HttpCode.INTERNAL_SERVER_ERROR ? "fail" : "error",
+    status: statusCode === HttpCode.INTERNAL_SERVER_ERROR ? 'fail' : 'error',
     code: statusCode,
     message: err.message,
   });
