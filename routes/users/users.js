8a6abd20d441742e8fb6a6bb59a7c1b03db88b43
Данатей <danatey40793@gmail.com>
2021-11-20 10:56:47 +0200

    убрал валидацию с юзера, исправил константы

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const {
-  validateUserSignup,
-  validateUserLogin,
-} = require('./validation');
+// const {
+//   validateUserSignup,
+//   validateUserLogin,
+// } = require('./validation');
 const {
   signup,
   login,
   logout,
-  currentUser } = require("../../controllers/users");
-const guard = require('../../helpers/guard');
-const loginLimit = require('../../helpers/rate-limit-login');
+  currentUser,
+} = require("../../controllers/users");
+const guard = require("../../helpers/guard");
+const loginLimit = require("../../helpers/rate-limit-login");
 
-router.post('/signup', validateUserSignup, signup);
-router.post('/login', loginLimit, validateUserLogin, login);
-router.post('/logout', guard, logout);
-router.get('/info', guard, currentUser);
+// router.post('/signup', validateUserSignup, signup);
+// router.post('/login', loginLimit, validateUserLogin, login);
+router.post("/signup", signup);
+router.post("/login", loginLimit, login);
+router.post("/logout", guard, logout);
+router.get("/info", guard, currentUser);
 
 module.exports = router;
