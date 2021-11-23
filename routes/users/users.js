0c0a907626f71c279450ba6ccc3905bff6682d35
Данатей <danatey40793@gmail.com>
2021-11-23 21:29:41 +0200

    Вернул валидацию рег., убрал валид. логин

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {
-  // validateUserSignup,
-  validateUserLogin,
-} = require("./validation");
+const { validateUserSignup, validateUserLogin } = require("./validation");
 const {
   signup,
   login,
@@ -14,8 +11,8 @@ const {
 const guard = require("../../helpers/guard");
 const loginLimit = require("../../helpers/rate-limit-login");
 
-router.post("/signup", signup);
-router.post("/login", loginLimit, validateUserLogin, login);
+router.post("/signup", validateUserSignup, signup);
+router.post("/login", loginLimit, login);
 router.post("/logout", guard, logout);
 router.get("/info", guard, currentUser);
 router.post("/refresh-tokens", refreshTokens);
