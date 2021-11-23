43846da95a3fc176d2c4144b609e0a2d5d81803f
serophiom <serophiom@gmail.com>
2021-11-23 19:59:11 +0200

    added google-auth-library and code

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,34 @@
 const db = require("../config/db");
 const app = require("../app");
+const express = require('express');
 require("dotenv").config();
+const { OAuth2Client } = require('google-auth-library');
 
 const PORT = process.env.PORT || 3001;
+const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+
+// const app = express();
+app.use(express.json());
+
+const users = [];
+
+function upsert(array, item) {
+  const i = array.findIndex((_item) => _item.email === item.email);
+  if (i > -1) array[i] = item;
+  else array.push(item);
+}
+
+app.post('/api/google-login', async (req, res) => {
+  const { token } = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  });
+  const { name, email, picture } = ticket.getPayload();
+  upsert(users, { name, email, picture });
+  res.status(201);
+  res.json({ name, email, picture });
+});
 
 db.then(() => {
   app.listen(PORT, async () => {
