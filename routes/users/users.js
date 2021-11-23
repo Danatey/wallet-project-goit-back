1f348415d35081a67a12f502f0716cbba954a9a7
Данатей <danatey40793@gmail.com>
2021-11-23 21:24:14 +0200

    Удалил валидацию юзера

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const {
-  validateUserSignup,
+  // validateUserSignup,
   validateUserLogin,
-} = require('./validation');
+} = require("./validation");
 const {
   signup,
   login,
   logout,
   currentUser,
   refreshTokens,
-  } = require("../../controllers/users");
-const guard = require('../../helpers/guard');
-const loginLimit = require('../../helpers/rate-limit-login');
+} = require("../../controllers/users");
+const guard = require("../../helpers/guard");
+const loginLimit = require("../../helpers/rate-limit-login");
 
-router.post('/signup', validateUserSignup, signup);
-router.post('/login', loginLimit, validateUserLogin, login);
-router.post('/logout', guard, logout);
-router.get('/info', guard, currentUser);
-router.post('/refresh-tokens', refreshTokens);
+router.post("/signup", signup);
+router.post("/login", loginLimit, validateUserLogin, login);
+router.post("/logout", guard, logout);
+router.get("/info", guard, currentUser);
+router.post("/refresh-tokens", refreshTokens);
 
 module.exports = router;
